44b9607c689cc8ff74fd7d93107357f2d9bbd0ed
Vishnu Korde <kewlvishnu@gmail.com>
2019-02-21 15:10:32 -0500

    changed name location

--- eks/flux/flux.sh
+++ eks/flux/flux.sh
@@ -3,6 +3,6 @@ helm repo add weaveworks https://weaveworks.github.io/flux
 if ! helm status flux; then
   helm install --wait --name flux \
     --namespace devops \
-    -f ${fluxValuesFile} \
+    -f /drone/src/eks/flux/values-preprod.yaml \
     weaveworks/flux
 fi
