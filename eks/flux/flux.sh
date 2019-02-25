44998003e27813b1d978c3c29c9d7a84a22f3032
Vishnu Korde <kewlvishnu@gmail.com>
2019-02-25 02:17:38 -0500

    added docs

--- eks/flux/flux.sh
+++ eks/flux/flux.sh
@@ -3,6 +3,10 @@ helm repo add weaveworks https://weaveworks.github.io/flux
 if ! helm status flux; then
   helm install --wait --name flux \
     --namespace devops \
-    -f /drone/src/eks/flux/values-preprod.yaml \
+    -f ../flux/values-preprod.yaml \
     weaveworks/flux
 fi
+# kubectl get customresourcedefinition
+# kubectl delete customresourcedefinition XXXXXXXXXXXXXX
+# helm delete --purge flux
+# this is to be done when the helmchart has been deleted, but still the CRD remains
