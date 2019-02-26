42778bb60f20a7ef7d0cfc75b808824b9160c6bb
Vishnu Korde <kewlvishnu@gmail.com>
2019-02-26 02:59:04 -0500

    added files fir flux

--- eks/flux/flux.sh
+++ eks/flux/flux.sh
@@ -2,6 +2,7 @@
 helm repo add weaveworks https://weaveworks.github.io/flux
 export AWS_DEFAULT_REGION=us-east-1
 if ! helm status flux; then
+  export CHECKPOINT_DISABLE=true
   helm install --wait --name flux \
     --namespace devops \
     -f ../flux/values-preprod.yaml \
@@ -12,3 +13,5 @@ fi
 # kubectl delete customresourcedefinition XXXXXXXXXXXXXX
 # helm delete --purge flux
 # this is to be done when the helmchart has been deleted, but still the CRD remains
+# NOTE: https://github.com/weaveworks/flux/blob/master/site/faq.md#how-do-i-use-my-own-deploy-key
+# NOTE2: https://github.com/stefanprodan/gitops-helm
