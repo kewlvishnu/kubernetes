d0c2cc6ff477e407c43d36f611f67d0e892f57a4
Vishnu Korde <kewlvishnu@gmail.com>
2019-03-27 15:28:54 -0400

    added some extra docs

--- eks/flux/flux.sh
+++ eks/flux/flux.sh
@@ -11,7 +11,9 @@ if ! helm status flux; then
 fi
 # kubectl get customresourcedefinition
 # kubectl delete customresourcedefinition XXXXXXXXXXXXXX
-# helm delete --purge flux
+# helm delete --purge <release_name>            #this is done to let flux redeploy, in this <release_name> is flux
+# to permanently delete and not let flux deploy: remove the HelmRelease.yaml file then run `kubectl -n <namespace_name> delete helmrelease/<release_name>`
 # this is to be done when the helmchart has been deleted, but still the CRD remains
 # NOTE: https://github.com/weaveworks/flux/blob/master/site/faq.md#how-do-i-use-my-own-deploy-key
 # NOTE2: https://github.com/stefanprodan/gitops-helm
+# fluxctl sync --k8s-fwd-ns <namespace_where_flux_is_installed> "Example: fluxctl sync --k8s-fwd-ns devops" This is used to force flux to run.   
